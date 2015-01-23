a1fe5564405c341dd9bbb9de997ecf359d8befdb
john layton <johnstewartlayton@gmail.com>
2015-01-23 21:34:46 +1100

    start to work on openlayers 3 web component

---
+++ openlayers-map.js
@@ -0,0 +1,57 @@
+"use strict";
+
+Polymer( 'openlayers-map', {
+  publish: {
+
+    /**
+     * The `latitude` attribute sets the map center.
+     *
+     * @attribute latitude
+     * @type number
+     */
+    latitude: {value: 0, reflect: true},
+
+    /**
+     * The `longitude` attribute sets the map center.
+     *
+     * @attribute longitude
+     * @type number
+     */
+    longitude: {value: 0, reflect: true},
+
+    /**
+     * The `zoom` attribute sets the map zoom.
+     *
+     * @attribute zoom
+     * @type number
+     */
+    zoom: {value: -1, reflect: true}
+  },
+
+  observe: {
+    latitude : 'view',
+    longitude: 'view',
+    zoom     : 'view'
+  },
+
+  domReady: function () {
+
+    var tile = new ol.layer.Tile( {
+      source: new ol.source.XYZ( {
+        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      } )
+    } );
+
+    var view = new ol.View( {
+      center: ol.proj.transform( [145.5, -37.8], 'EPSG:4326', 'EPSG:3857' ),
+      zoom  : 9
+    } );
+
+    var map = new ol.Map( {target: this.$.map, layers: [tile], view: view} );
+  },
+
+  created: function () {
+    console.log( "Created ..." );
+  }
+
+} );
